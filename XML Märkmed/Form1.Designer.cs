7020a9c200f2d4394b1a71549ef24acb9a7dd62c
ViktorOnegin <Viktor.Onegin@tptlive.ee>
2018-05-07 17:40:26 +0300

    harjutus

---
+++ XML Märkmed/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace XML_Märkmed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UuedMärkmed = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Pealkiri1 = new System.Windows.Forms.Label();
+            this.Sisu1 = new System.Windows.Forms.Label();
+            this.LisaMärkmed = new System.Windows.Forms.Button();
+            this.Pealkiri2 = new System.Windows.Forms.Label();
+            this.Sisu2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.KustutaMärkmed = new System.Windows.Forms.Button();
+            this.Pealkiri3 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // UuedMärkmed
+            // 
+            this.UuedMärkmed.Location = new System.Drawing.Point(12, 0);
+            this.UuedMärkmed.Name = "UuedMärkmed";
+            this.UuedMärkmed.Size = new System.Drawing.Size(121, 33);
+            this.UuedMärkmed.TabIndex = 0;
+            this.UuedMärkmed.Text = "Uued Märkmed";
+            this.UuedMärkmed.UseVisualStyleBackColor = true;
+            this.UuedMärkmed.Click += new System.EventHandler(this.UusMärkmik_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(86, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(331, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Pealkiri1
+            // 
+            this.Pealkiri1.AutoSize = true;
+            this.Pealkiri1.Location = new System.Drawing.Point(39, 42);
+            this.Pealkiri1.Name = "Pealkiri1";
+            this.Pealkiri1.Size = new System.Drawing.Size(41, 13);
+            this.Pealkiri1.TabIndex = 4;
+            this.Pealkiri1.Text = "Pealkiri";
+            // 
+            // Sisu1
+            // 
+            this.Sisu1.AutoSize = true;
+            this.Sisu1.Location = new System.Drawing.Point(53, 68);
+            this.Sisu1.Name = "Sisu1";
+            this.Sisu1.Size = new System.Drawing.Size(27, 13);
+            this.Sisu1.TabIndex = 5;
+            this.Sisu1.Text = "Sisu";
+            // 
+            // LisaMärkmed
+            // 
+            this.LisaMärkmed.Location = new System.Drawing.Point(7, 91);
+            this.LisaMärkmed.Name = "LisaMärkmed";
+            this.LisaMärkmed.Size = new System.Drawing.Size(126, 33);
+            this.LisaMärkmed.TabIndex = 6;
+            this.LisaMärkmed.Text = "Lisa Märkmed";
+            this.LisaMärkmed.UseVisualStyleBackColor = true;
+            this.LisaMärkmed.Click += new System.EventHandler(this.LisaMärkmed_Click);
+            // 
+            // Pealkiri2
+            // 
+            this.Pealkiri2.AutoSize = true;
+            this.Pealkiri2.Location = new System.Drawing.Point(39, 127);
+            this.Pealkiri2.Name = "Pealkiri2";
+            this.Pealkiri2.Size = new System.Drawing.Size(41, 13);
+            this.Pealkiri2.TabIndex = 7;
+            this.Pealkiri2.Text = "Pealkiri";
+            // 
+            // Sisu2
+            // 
+            this.Sisu2.AutoSize = true;
+            this.Sisu2.Location = new System.Drawing.Point(53, 156);
+            this.Sisu2.Name = "Sisu2";
+            this.Sisu2.Size = new System.Drawing.Size(27, 13);
+            this.Sisu2.TabIndex = 8;
+            this.Sisu2.Text = "Sisu";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(86, 127);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(331, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(86, 153);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(331, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // KustutaMärkmed
+            // 
+            this.KustutaMärkmed.Location = new System.Drawing.Point(7, 179);
+            this.KustutaMärkmed.Name = "KustutaMärkmed";
+            this.KustutaMärkmed.Size = new System.Drawing.Size(126, 33);
+            this.KustutaMärkmed.TabIndex = 11;
+            this.KustutaMärkmed.Text = "Kustuta Märkmed";
+            this.KustutaMärkmed.UseVisualStyleBackColor = true;
+            this.KustutaMärkmed.Click += new System.EventHandler(this.KustutaMärkmed_Click);
+            // 
+            // Pealkiri3
+            // 
+            this.Pealkiri3.AutoSize = true;
+            this.Pealkiri3.Location = new System.Drawing.Point(39, 215);
+            this.Pealkiri3.Name = "Pealkiri3";
+            this.Pealkiri3.Size = new System.Drawing.Size(41, 13);
+            this.Pealkiri3.TabIndex = 12;
+            this.Pealkiri3.Text = "Pealkiri";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(86, 212);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(331, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 301);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.Pealkiri3);
+            this.Controls.Add(this.KustutaMärkmed);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Sisu2);
+            this.Controls.Add(this.Pealkiri2);
+            this.Controls.Add(this.LisaMärkmed);
+            this.Controls.Add(this.Sisu1);
+            this.Controls.Add(this.Pealkiri1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.UuedMärkmed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UuedMärkmed;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label Pealkiri1;
+        private System.Windows.Forms.Label Sisu1;
+        private System.Windows.Forms.Button LisaMärkmed;
+        private System.Windows.Forms.Label Pealkiri2;
+        private System.Windows.Forms.Label Sisu2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button KustutaMärkmed;
+        private System.Windows.Forms.Label Pealkiri3;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ XML Märkmed/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XML_Märkmed
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UusMärkmik_Click(object sender, EventArgs e)
+        {
+            XmlTextWriter märkmed = new XmlTextWriter("D:\\programs\\Visual Studio 2017\\XML Märkmed\\XML Märkmed\\bin\\Debug\\Märkmed.xml" , Encoding.UTF8);
+            märkmed.Formatting = Formatting.Indented;
+            märkmed.WriteStartElement("Märkmik");
+            märkmed.WriteStartElement("Uued Märkmed");
+            märkmed.WriteStartElement("Pealkiri");
+            märkmed.WriteString(textBox1.Text);
+            märkmed.WriteEndElement();
+            märkmed.WriteStartElement("Sisu");
+            märkmed.WriteString(textBox2.Text);
+            märkmed.WriteEndElement();
+            märkmed.WriteEndElement();
+            märkmed.WriteEndElement();
+            märkmed.Close();
+        }
+
+        private void LisaMärkmed_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("D:\\programs\\Visual Studio 2017\\XML Märkmed\\XML Märkmed\\bin\\Debug\\Märkmed.xml");
+
+            XmlNode märkmik = doc.CreateElement("Märkmik");
+            XmlNode pealkiri2 = doc.CreateElement("Pealkiri");
+            pealkiri2.InnerText = textBox3.Text;
+            märkmik.AppendChild(pealkiri2);
+
+            XmlNode sisu2 = doc.CreateElement("Sisu");
+            sisu2.InnerText = textBox4.Text;
+            märkmik.AppendChild(sisu2);
+            doc.DocumentElement.AppendChild(märkmik);
+            doc.Save("D:\\programs\\Visual Studio 2017\\XML Märkmed\\XML Märkmed\\bin\\Debug\\Märkmed.xml");
+        }
+
+        private void KustutaMärkmed_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("D:\\programs\\Visual Studio 2017\\XML Märkmed\\XML Märkmed\\bin\\Debug\\Märkmed.xml");
+
+            foreach (XmlNode xmlNode in doc.SelectNodes("Märkmik/Uued Märkmed"))
+                if (xmlNode.SelectSingleNode("Pealkiri").InnerText == textBox5.Text) xmlNode.ParentNode.RemoveChild(xmlNode);
+
+            doc.Save("D:\\programs\\Visual Studio 2017\\XML Märkmed\\XML Märkmed\\bin\\Debug\\Märkmed.xml");
+        }
+    }
+}
